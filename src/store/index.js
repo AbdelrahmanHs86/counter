d0064d5bc0bde51befc642dd696085970c9080f5
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-09 20:47:11 +0300

    feat: Include multislices

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './reducers/counterSlice';
+import authReducer from './reducers/authSlice';
 
-const store = configureStore({ reducers: { counterReducer } });
-
-
-
+const store = configureStore({ reducer: { counter: counterReducer, auth: authReducer } },);
 
 export default store;
